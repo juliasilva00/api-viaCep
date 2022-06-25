415b71d634e60ecc55aed83abc3fdd1408f22ed3
juliasilva00 <juliacajuru@gmail.com>
2022-06-25 18:27:57 -0300

    cosuming an API with JS

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const preencherFormulario = (endereco) => {
+    document.getElementById('endereco').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+}
+const pesquisarCep = async() => {
+    const cep = document.getElementById('cep').value;
+    const url = `http://viacep.com.br/ws/${cep}/json/`;
+    const dadosPromise = await fetch(url);
+    const endereco = await dadosPromise.json();
+    if (endereco.hasOwnProperty('erro')){
+        console.log(endereco);
+    } else{
+        preencherFormulario(endereco);
+    }
+
+}
+
+
+document.getElementById('cep').addEventListener('focusout', pesquisarCep);
